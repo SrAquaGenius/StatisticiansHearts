48bd16d47ee306ca42d0bce55d9346e92aaa4807
SrAquaGenius <dimoisivic@gmail.com>
2024-11-13 17:30:24 +0000

    feat: Created inital versions of Card, Suit and Rank classes

--- src/main/java/game/Main.java
+++ src/main/java/game/Main.java
@@ -1,7 +1,13 @@
 package game;
 
+import model.Card;
+import model.Rank;
+import model.Suit;
+
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Hearts Game!");
+		Card card = new Card(Suit.HEARTS, Rank.QUEEN);
+		System.out.println(card);  // Outputs "QUEEN of HEARTS"
 	}
 }
---
+++ src/main/java/model/Card.java
@@ -0,0 +1,28 @@
+package model;
+
+public class Card {
+
+	private Suit _suit;
+	private Rank _rank;
+
+	/* ---------------------------- Constructor ---------------------------- */
+	public Card(Suit suit, Rank rank) {
+		this._suit = suit;
+		this._rank = rank;
+	}
+
+	/* ----------- Getters, Setters and useful general functions ----------- */
+	public Suit getSuit() {
+		return _suit;
+	}
+
+	public Rank getRank() {
+		return _rank;
+	}
+
+	/* ----------------------------- toString ------------------------------ */
+	@Override
+	public String toString() {
+		return _rank + " of " + _suit;
+	}
+}
